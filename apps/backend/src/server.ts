5cf01b961cf4eaff937d8c0cec706dbbb8369443
loyihalar97 <hasanboy19911997@gmail.com>
2026-08-11 10:41:17 +0500

    Update server.ts

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,13 +3,28 @@ import { config } from "./core/config";
 import { logger } from "./core/logger";
 import { prisma } from "./core/database/prisma";
 
+process.on("uncaughtException", (err) => {
+  logger.error({ err }, "Uncaught exception - process is dying");
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  logger.error({ err }, "Unhandled rejection - process is dying");
+  process.exit(1);
+});
+
 async function main() {
   await prisma.$connect();
   logger.info("Database connected");
 
   const app = createApp();
-  app.listen(config.port, () => {
-    logger.info(`Backend server running on port ${config.port} (${config.env})`);
+  const server = app.listen(config.port, "0.0.0.0", () => {
+    logger.info(`Backend server running on 0.0.0.0:${config.port} (${config.env})`);
+  });
+
+  server.on("error", (err) => {
+    logger.error({ err }, "HTTP server failed to start");
+    process.exit(1);
   });
 }
 
